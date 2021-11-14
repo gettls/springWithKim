d723d2070cd4d43070bec0f727525c3adddfcb08
gettls <gettls@daum.net>
2021-11-14 17:30:06 +0900

    jpa

--- jpashop/src/main/java/jpabook/jpashop/domain/Member.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,51 +1,31 @@
 package jpabook.jpashop.domain;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
+@Getter @Setter
 public class Member {
-	
-	@Id @GeneratedValue
-	@Column(name="MEMBER_ID")
-	private Long id;
-	private String name;
-	private String city;
-	private String street;
-	private String zipcode;
-	
-	
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getCity() {
-		return city;
-	}
-	public void setCity(String city) {
-		this.city = city;
-	}
-	public String getStreet() {
-		return street; 
-	}
-	public void setStreet(String street) {
-		this.street = street;
-	}
-	public String getZipcode() {
-		return zipcode;
-	}
-	public void setZipcode(String zipcode) {
-		this.zipcode = zipcode;
-	}
+
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    @NotEmpty
+    private String name;
+
+    @Embedded
+    private Address address;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<>();
+
 }
